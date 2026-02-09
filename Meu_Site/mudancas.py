b99bf845d455d1c3d0f4608b4e65413679432f88
viccenzo-zaffari <viccenzo.zaffari12@gmail.com>
2026-02-09 19:34:40 -0300

    Add files via upload

---
+++ Meu_Site/mudancas.py
@@ -0,0 +1,14 @@
+#MUNDAÇAS NA PARTE DE PRODUTOS, ADICIONAR MAIS FOTOS
+#{
+#  "nome": "Creatina em Goma Tutti Frutti",
+#  "descricao": "Creatina prática e saborizada para o dia a dia.",
+#  "preco": "R$ 79,90",
+#  "parcelado": "ou 3x de R$ 26,63",
+#  "imagens": [
+#    "assets/imagens/creatina1.jpeg",
+#    "assets/imagens/creatina2.jpeg",
+#    "assets/imagens/creatina3.jpeg"
+#  ],
+#  "link": "https://checkout-do-parceiro.com/produto1"
+#}
+
